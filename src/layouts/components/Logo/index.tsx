543a575d685fc0732e646baefcf96f3c014705e1
yanzhiguang <yanzhiguang@jingxunsoft.com>
2023-06-09 11:58:54 +0800

    添加logo导航

--- src/layouts/components/Logo/index.tsx
+++ src/layouts/components/Logo/index.tsx
@@ -3,14 +3,17 @@ import { ReactComponent as LogoIcon } from '@/icons/svg/logo.svg'
 import { theme } from 'antd'
 import { connect } from 'react-redux'
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { HOME_URL } from '@/config/config'
 
 const Logo: FC<{ isCollapse: boolean }> = (props) => {
   const {
     token: { colorPrimary }
   } = theme.useToken()
   const { isCollapse } = props
+  const navigate = useNavigate()
   return (
-    <div className={styles.logo}>
+    <div className={styles.logo} onClick={() => navigate(HOME_URL)}>
       <LogoIcon className={styles.icon} />
       {!isCollapse && (
         <span
